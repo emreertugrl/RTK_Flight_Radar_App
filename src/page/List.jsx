d9f53aad72c85c2cd1b65121b2af739817f80f9d
emreertugrl <emrertugrl@gmail.com>
2024-09-24 20:06:18 +0300

    pagination işlemleri tamamlandı

--- src/page/List.jsx
+++ src/page/List.jsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import Loader from "../components/Loader";
 import Error from "./../components/Error";
+import ReactPaginate from "react-paginate";
 
 const List = ({ setDetailId }) => {
   const { isLoading, error, flights } = useSelector((store) => store.flight);
+  // kaçıncı elemandan itibaren kesileck
+  const [start, setStart] = useState(0);
+  // ekranda gösterlecek eleman sayısı
+  const perPage = 10;
+  // kaçıncı elemana kadar kesilecek
+  const end = start + perPage;
 
+  // slice methodu ile başlangıç ve bitiş
+  const currFlights = flights.slice(start, end);
+
+  // toplam sayfa sayısı
+  const total = Math.ceil(flights.length / perPage);
+  // yeni sayfaya tıklanınca
+  const handlePageClick = (event) => {
+    // başlangıç state'ini güncelle
+    setStart(event.selected * perPage);
+  };
   if (isLoading)
     return (
       <div className="list-loader">
@@ -27,7 +44,7 @@ const List = ({ setDetailId }) => {
         </thead>
 
         <tbody>
-          {flights.map((flight) => (
+          {currFlights.map((flight) => (
             <tr>
               <td>{flight.id}</td>
               <td>{flight.code}</td>
@@ -45,6 +62,17 @@ const List = ({ setDetailId }) => {
           ))}
         </tbody>
       </table>
+
+      <ReactPaginate
+        className="pagination"
+        breakLabel="..."
+        nextLabel="İleri >"
+        previousLabel="< Geri"
+        onPageChange={handlePageClick}
+        pageRangeDisplayed={5}
+        pageCount={total}
+        renderOnZeroPageCount={true}
+      />
     </div>
   );
 };
